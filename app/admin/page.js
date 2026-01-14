331984d4745e8ac16908cb802090c6a91dbe54ee
vivekml251me163 <vivekml2007@gmail.com>
2026-01-15 00:40:03 +0530

    8th

--- app/admin/page.js
+++ app/admin/page.js
@@ -1,9 +1,10 @@
 import { getServerSession } from "next-auth"
 import AdminClient from "./AdminClient"
+import { handler } from "@/app/api/auth/[...nextauth]/route"
 
 
 export default async function AdminPage() {
-  const session = await getServerSession()
+  const session = await getServerSession(handler)
 
   return <AdminClient session={session} />
 }
